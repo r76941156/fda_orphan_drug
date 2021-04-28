f3cc9853a7b27260ceaae822643c7b6b731553a0
Sander Su <wuchen_sue@hotmail.com>
2021-04-28 12:20:12 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,15 @@
 import pandas as pd
 import os
 import csv
-
-#print(pd.__version__)
+import numpy as np
 
 from biothings import config
-from biothings.utils.dataload import dict_convert
+from biothings.utils.dataload import dict_convert, dict_sweep
 logging = config.logger
 
 def load_data(data_folder):
     infile = os.path.join(data_folder,"compound.csv")
     assert os.path.exists(infile)
-    #dat = pd.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict('records')
-    dat = pd.read_csv(infile,sep="\t",quoting=csv.QUOTE_NONE).to_dict('records')
-    #results = {}
-    #for rec in dat:
-        #print(rec)
-        #print("=======")
-
-#load_data("./")
+    dat = pd.read_csv(infile).to_dict('records')
+    for rec in dat:
+         rec = dict_sweep(rec,vals=[np.nan])
