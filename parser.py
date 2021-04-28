04e6b785e8044d8e989b7207e3610ac074258ee3
Sander Su <wuchen_sue@hotmail.com>
2021-04-28 15:47:57 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -10,7 +10,17 @@ logging = config.logger
 def load_data(data_folder):
     infile = os.path.join(data_folder,"compound.csv")
     assert os.path.exists(infile)
-    dat = pd.read_csv(infile,sep="\t").to_dict('records')
+    #dat = pd.read_csv(infile,sep="\t").to_dict('records')
+    dat = pd.read_csv(infile).to_dict('records')
+    results={}
     for rec in dat:
          rec = dict_sweep(rec,vals=[np.nan])
-    return dat        
+    for rec in dat:
+         _id=rec['_id']
+         del rec['_id']
+         results.setdefault(_id,[]).append(rec)
+    #print(results['GJPICJJJRGTNOD-UHFFFAOYSA-N'])
+
+    for _id,docs in results.items():
+        doc = {"_id": _id, "Orphan Drug Designations" : docs}
+        yield doc
