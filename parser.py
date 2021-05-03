562dc175742c0479dbdfc761edc11763d7eb5cc3
Sander Su <wuchen_sue@hotmail.com>
2021-05-03 09:42:02 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,11 +4,8 @@ import json
 
 def load_data(data_folder):
     infile = os.path.join(data_folder,"data.json")
-    f = open(infile)
-    # returns JSON object as 
-    # a dictionary
-    data = json.load(f)
-    f.close()
+    with open(infile) as f:
+        data = json.load(f)
     for doc in data:
         yield doc
 
